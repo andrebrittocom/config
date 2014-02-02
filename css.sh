69a118aa777e0c675d5439d4c8be17dcdd147e23
André Britto <contato@andrebritto.com>
2014-02-02 10:39:31 -0200

    Css

--- css.sh
+++ css.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Teste"
+echo "Install Sass and Compass..."
+sudo apt-get install ruby1.9.1
+sudo gem install sass
+sudo gem install compass
+
+echo "Finish!"
+echo "Use: compass --watch [dir]"
