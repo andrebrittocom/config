240ef2167d3863a146edb4c8a8f016292225d095
André Britto <contato@andrebritto.com>
2014-02-02 11:15:10 -0200

    Php

--- php.sh
+++ php.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+echo "Install Apache2, php5 and mysql-server..."
+sudo apt-get install apache2
+sudo apt-get install mysql-server
+sudo apt-get install php5
+sudo apt-get install php5-mysql
+sudo a2enmod rewrite
+sudo cp /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/
+sudo service apache2 restart
+
+echo "Finish!"
+
+echo "Edit this: sudo vim /etc/apache2/sites-available/default"
+echo "Change: DocumentRoot /var/www -> AllowOverride None ->to-> AllowOverride All"
+echo "sudo service apache2 restart"
