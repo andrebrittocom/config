9af7298b34f1ed70d0880d5f627c6fc8244692f9
André Britto <contato@andrebritto.com>
2014-05-10 16:28:22 -0300

    Mysql

--- mysql.sh
+++ mysql.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "Configurando Mysql Server..."
+echo "Config. Mysql Server..."
 
 sudo apt-get install mysql-server
+
+echo "Config. new user..."
 mysql -u root -p -e "CREATE USER 'giuser'@'127.0.0.1' IDENTIFIED BY 'P4ssw0rd';"
 mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO 'giuser'@'127.0.0.1' WITH GRANT OPTION;"
 
